a7d6bbc3bd3eebbb4bf33c768973d6ff45c3451c
PushkarDesai-06 <pushkarjdesai@gmail.com>
2025-07-27 13:39:49 +0530

    Rename client.java to RealEstateService.java and refactor methods to include database connection parameters. Removed main method and env dependency from the clint file

--- src/main/java/RealEstateMaven/RealEstateService.java
+++ src/main/java/RealEstateMaven/RealEstateService.java
@@ -8,36 +8,11 @@ import java.util.ArrayList;
 
 import RealEstateMaven.Classes.*;
 import RealEstateMaven.Queries.SqlQueries;
-import io.github.cdimascio.dotenv.Dotenv;
 
-
-public class client {
-  final static Dotenv dotenv = Dotenv.load();
-
-  static String db_url = dotenv.get("DB_URL");
-  static String db_user = dotenv.get("DB_USER");
-  static String db_pass = dotenv.get("DB_PASSWORD");
-
-  public static void main(String[] args) throws RuntimeException {
-    if(db_url == null || db_user == null || db_pass == null){
-      System.out.println("Check your .env file");
-      return;
-    }
-
-    ArrayList<AgentPerformanceClass> list = getAgentPerformanceByClosedDeals();
-    for (AgentPerformanceClass var : list) {
-      System.out.println(var.toString());
-    }
-
-    System.exit(0);
-
-  }
+public class RealEstateService {
 
   public static Connection getConnection(String db_url, String db_username, String db_pass) throws RuntimeException {
-
     try {
-
-      // Class.forName("com.mysql.cj.jdbc.Driver");
       return DriverManager.getConnection(db_url, db_username, db_pass);
     } catch (Exception e) {
       System.out.println("Error : \n" + e.getMessage());
@@ -48,8 +23,7 @@ public class client {
   // Functions for Requirements
 
   // 1. Inquiry Response Time metrics per agent
-
-  public static ArrayList<ResponseTimeMetricsClass> getResponseTimeMetrics() throws RuntimeException {
+  public static ArrayList<ResponseTimeMetricsClass> getResponseTimeMetrics(String db_url, String db_user, String db_pass) throws RuntimeException {
     ArrayList<ResponseTimeMetricsClass> res = new ArrayList<>();
     String query = SqlQueries.GET_RESPONSE_TIME_METRICS;
     try (
@@ -57,42 +31,33 @@ public class client {
         PreparedStatement pst = conn.prepareStatement(query);) {
       try (ResultSet rs = pst.executeQuery()) {
         while (rs.next()) {
-
           res.add(new ResponseTimeMetricsClass(
               rs.getString("id"),
               rs.getString("name"),
               rs.getString("avgTime")
-
           ));
         }
-
         return res;
       }
-
     } catch (SQLException e) {
       throw new RuntimeException("Error Fetching", e);
     }
   }
 
   // 2. Active listings by neighborhood and property type.
-
-  public static ArrayList<ActiveListingsClass> getActiveListings(String neighbourhood, String type)
+  public static ArrayList<ActiveListingsClass> getActiveListings(String neighbourhood, String type, String db_url, String db_user, String db_pass)
       throws RuntimeException {
     ArrayList<ActiveListingsClass> res = new ArrayList<>();
     String query = SqlQueries.GET_ACTIVE_LISTINGS;
     try (
-
         Connection conn = getConnection(db_url, db_user, db_pass);
         PreparedStatement pst = conn.prepareStatement(query);
-
     ) {
       pst.setString(1, neighbourhood);
       pst.setString(2, type);
 
       try (ResultSet rs = pst.executeQuery()) {
-
         while (rs.next()) {
-
           res.add(new ActiveListingsClass(
               rs.getString("id"),
               rs.getString("neighborhood"),
@@ -103,52 +68,39 @@ public class client {
               rs.getString("listing_price"),
               rs.getString("address")));
         }
-
       }
       return res;
-
     } catch (SQLException e) {
       throw new RuntimeException("Error Fetching", e);
     }
   }
 
   // 3. Avg time on market
-
-  public static AvgTimeOnMarketClass getAvgTimeOnMarket() throws RuntimeException {
-
+  public static AvgTimeOnMarketClass getAvgTimeOnMarket(String db_url, String db_user, String db_pass) throws RuntimeException {
     String query = SqlQueries.GET_AVG_TIME_ON_MARKET;
     try (
-
         Connection conn = getConnection(db_url, db_user, db_pass);
         PreparedStatement pst = conn.prepareStatement(query);) {
-
       try (ResultSet rs = pst.executeQuery();) {
-
         int avgTime = 0;
         while (rs.next()) {
           avgTime = rs.getInt("avg_time_on_market");
         }
-          return new AvgTimeOnMarketClass(avgTime);
+        return new AvgTimeOnMarketClass(avgTime);
       }
-
     } catch (SQLException e) {
       throw new RuntimeException("Failed Fetching", e);
     }
-
   }
 
   // 4. Price trend analysis for a region
-
-  public static ArrayList<PriceTrendClass> getPriceTrend() throws RuntimeException {
+  public static ArrayList<PriceTrendClass> getPriceTrend(String db_url, String db_user, String db_pass) throws RuntimeException {
     String query = SqlQueries.GET_PRICE_TREND;
     ArrayList<PriceTrendClass> list = new ArrayList<>();
     try (
-
         Connection conn = getConnection(db_url, db_user, db_pass);
         PreparedStatement pst = conn.prepareStatement(query);) {
-
       try (ResultSet rs = pst.executeQuery();) {
-
         while (rs.next()) {
           list.add(new PriceTrendClass(
                   rs.getString("avg_price"),
@@ -157,25 +109,19 @@ public class client {
         }
         return list;
       }
-
     } catch (SQLException e) {
       throw new RuntimeException("Failed Fetching", e);
     }
-
   }
 
   // 5. Inquiries per property last month
-
-  public static ArrayList<InqPerPropLastMonthClass> getInquiriesPerPropertiesLastMonth() throws RuntimeException {
+  public static ArrayList<InqPerPropLastMonthClass> getInquiriesPerPropertiesLastMonth(String db_url, String db_user, String db_pass) throws RuntimeException {
     ArrayList<InqPerPropLastMonthClass> res = new ArrayList<>();
     String query = SqlQueries.GET_INQUIRIES_PER_PROPERTY_LAST_MONTH;
     try (
-
         Connection conn = getConnection(db_url, db_user, db_pass);
         PreparedStatement pst = conn.prepareStatement(query);) {
-
       try (ResultSet rs = pst.executeQuery();) {
-
         while (rs.next()) {
           res.add(
               new InqPerPropLastMonthClass(
@@ -189,25 +135,19 @@ public class client {
         }
         return res;
       }
-
     } catch (SQLException e) {
       throw new RuntimeException("Failed Fetching", e);
     }
-
   }
 
   // 6. Offers pending acceptance.
-
-  public static ArrayList<OffersPendingAcceptanceClass> getOffersPendingAcceptance() throws RuntimeException {
+  public static ArrayList<OffersPendingAcceptanceClass> getOffersPendingAcceptance(String db_url, String db_user, String db_pass) throws RuntimeException {
     ArrayList<OffersPendingAcceptanceClass> res = new ArrayList<>();
     String query = SqlQueries.GET_OFFERS_PENDING_ACCEPTANCE;
     try (
-
         Connection conn = getConnection(db_url, db_user, db_pass);
         PreparedStatement pst = conn.prepareStatement(query);) {
-
       try (ResultSet rs = pst.executeQuery();) {
-
         while (rs.next()) {
           res.add(
               new OffersPendingAcceptanceClass(
@@ -223,36 +163,32 @@ public class client {
         }
         return res;
       }
-
     } catch (SQLException e) {
       throw new RuntimeException("Failed Fetching", e);
     }
-
   }
 
-    // 7. Agent performance by closed deals
-
-    public static ArrayList<AgentPerformanceClass> getAgentPerformanceByClosedDeals() throws RuntimeException {
-      ArrayList<AgentPerformanceClass> res = new ArrayList<>();
-      String query = SqlQueries.GET_AGENT_PERFORMANCE_BY_CLOSED_DEALS;
-      try(
-        Connection conn = getConnection(db_url, db_user, db_pass);
-        PreparedStatement pst = conn.prepareStatement(query);
-        ){
-        try(ResultSet rs = pst.executeQuery()){
-          while(rs.next()){
-            res.add(new AgentPerformanceClass(
-                rs.getString("id"),
-                rs.getString("name"),
-                rs.getString("phone_number"),
-                rs.getInt("properties_sold")
-            ));
-          }
+  // 7. Agent performance by closed deals
+  public static ArrayList<AgentPerformanceClass> getAgentPerformanceByClosedDeals(String db_url, String db_user, String db_pass) throws RuntimeException {
+    ArrayList<AgentPerformanceClass> res = new ArrayList<>();
+    String query = SqlQueries.GET_AGENT_PERFORMANCE_BY_CLOSED_DEALS;
+    try(
+      Connection conn = getConnection(db_url, db_user, db_pass);
+      PreparedStatement pst = conn.prepareStatement(query);
+      ){
+      try(ResultSet rs = pst.executeQuery()){
+        while(rs.next()){
+          res.add(new AgentPerformanceClass(
+              rs.getString("id"),
+              rs.getString("name"),
+              rs.getString("phone_number"),
+              rs.getInt("properties_sold")
+          ));
         }
-      }catch (SQLException e){
-        throw new RuntimeException("Failed Fetching", e);
       }
-        return res;
+    }catch (SQLException e){
+      throw new RuntimeException("Failed Fetching", e);
     }
-
+      return res;
+  }
 }
